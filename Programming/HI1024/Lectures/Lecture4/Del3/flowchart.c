f8dddf5ff71541ea6502be50b21675fbeebde033
marminge <viktor@marminge.se>
2025-09-01 18:14:29 +0200

    Fixed lecture4 part 3.

--- Programming/HI1024/Lectures/Lecture4/Del3/flowchart.c
+++ Programming/HI1024/Lectures/Lecture4/Del3/flowchart.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main () {
+int main() {
   int a = 100, b = 1;
-  
-  while(1) {
-    if (a <= b) {
-      printf("\n");
-    printf("SLUT  a: %d, b: %d\n", a, b);
-      exit(0);
-    }
-  
-    printf("a: %d, b: %d\n", a, b);
 
-    if (a/b > 20) {
+  while (a > b) {
+    printf("%d, %d\n", a, b);
+    if (a / b > 20) {
       a /= 2;
-      continue;
-    } else if (b < a/2){
-      b += 2;
-      printf("b: %d\n", b);
     } else {
-      b += 1;
-      printf("b: %d\n", b);
+      while (b < a / 2) {
+        b++;
+        printf("%d\n", b);
+      }
+      b++;
     }
   }
-
+  printf("%d, %d\n", a, b);
 
   return 0;
 }
